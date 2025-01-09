c369516b6a97c0e85db28d0e6eceb2725f7d7712
Sabarishwaran-B <sabarish260904@gmail.com>
2025-01-09 11:18:36 +0530

    File added

---
+++ script.js
@@ -0,0 +1,7 @@
+/*--------------typing animation */
+var typed = new Typed(".typing", {
+    strings: ["", "Pre-Final Year Student", "Web Designer", "Web Developer", "3rd Year Student"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
